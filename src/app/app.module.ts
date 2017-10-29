1cb392a5a14e500ab2ff70fea9518cda4cb78b20
Jorge Vergara <javebratt@gmail.com>
2017-10-29 14:17:53 -0500

    Creates all pages and providers

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,38 +3,33 @@ import { BrowserModule } from '@angular/platform-browser';
 import { IonicApp, IonicModule, IonicErrorHandler } from 'ionic-angular';
 import { MyApp } from './app.component';
 
-import { AboutPage } from '../pages/about/about';
-import { ContactPage } from '../pages/contact/contact';
-import { HomePage } from '../pages/home/home';
-import { TabsPage } from '../pages/tabs/tabs';
-
 import { StatusBar } from '@ionic-native/status-bar';
 import { SplashScreen } from '@ionic-native/splash-screen';
+import { AuthProvider } from '../providers/auth/auth';
+import { TeamProvider } from '../providers/team/team';
+
+import { AngularFireModule } from 'angularfire2';
+import { AngularFireAuthModule } from 'angularfire2/auth';
+import { AngularFireDatabaseModule } from 'angularfire2/database';
+import { firebaseConfig } from './credentials';
 
 @NgModule({
-  declarations: [
-    MyApp,
-    AboutPage,
-    ContactPage,
-    HomePage,
-    TabsPage
-  ],
+  declarations: [MyApp],
   imports: [
     BrowserModule,
-    IonicModule.forRoot(MyApp)
+    IonicModule.forRoot(MyApp),
+    AngularFireModule.initializeApp(firebaseConfig),
+    AngularFireAuthModule,
+    AngularFireDatabaseModule
   ],
   bootstrap: [IonicApp],
-  entryComponents: [
-    MyApp,
-    AboutPage,
-    ContactPage,
-    HomePage,
-    TabsPage
-  ],
+  entryComponents: [MyApp],
   providers: [
     StatusBar,
     SplashScreen,
-    {provide: ErrorHandler, useClass: IonicErrorHandler}
+    { provide: ErrorHandler, useClass: IonicErrorHandler },
+    AuthProvider,
+    TeamProvider
   ]
 })
 export class AppModule {}
---
+++ src/app/credentials.ts
@@ -0,0 +1,8 @@
+export const firebaseConfig = {
+  apiKey: 'AIzaSyBVXxy_CqyllPB8DoGN_h9se-TocSepwI0',
+  authDomain: 'task-manager-tutorial.firebaseapp.com',
+  databaseURL: 'https://task-manager-tutorial.firebaseio.com',
+  projectId: 'task-manager-tutorial',
+  storageBucket: '',
+  messagingSenderId: '593853471870'
+};
